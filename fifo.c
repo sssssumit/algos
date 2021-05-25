434e33520e5cf6630621904851cc594cc8c242d6
sssssumit <84775116+sssssumit@users.noreply.github.com>
2021-05-25 10:11:47 +0000

    Add files via upload

---
+++ fifo.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int a[100],b[100],i,n,z,f,j,pf,h,temp;
+void main(){
+scanf("%d",&n);
+scanf("%d",&f);
+for(i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+
+for(i=0;i<f;i++){		
+b[i]=-1;
+}
+i=0;j=0;h=0;
+
+
+
+while(i<n){
+if(b[i]=-1 && i<f ){	
+b[i]=a[i];
+pf++;					
+}
+else{
+z=0;
+for(j=0;j<f;j++){		
+	if(b[j]==a[i]){		
+	h++; 				
+	}
+	else{
+	z++;			
+	}
+}
+if(z==f){					
+	pf++;				
+	for(j=0;j<f-1;j++){		
+	temp=b[j];
+	b[j]=b[j+1];
+	b[j+1]=temp;
+	}
+	b[f-1]=a[i];		
+}
+}
+printf("\n CF:  %d \t %d \t %d \n",b[2],b[1],b[0]);
+i++;
+}
+printf("\n frame :");	
+for(i=0;i<f;i++){
+printf("\n b[%d] = %d",i,b[i]); 
+}
+printf("\n PF = %d ",pf);
+printf("\n Hit = %d ",h);
+printf("\n");
+}
